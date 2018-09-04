a282cac79b9aff0d41887f36b2ced37985ac6488
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-09-04 13:28:46 -0400

    change things, see what happens

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,34 +10,34 @@ app.use(bodyParser.json());
 
 module.exports = function(app) {
     // this is the get route from express
-    app.get("api/friends", function (request, response) {
-        response.json(newBestFriends);
+    app.get("/api/friends", function (request, response) {
+        response.json(friendsArray);
     });
 
     // this is thet post route from express
-    app.post("api/friends", function(request, response) {
+    app.post("/api/friends", function(request, response) {
         var newUser = request.body;
-        var newUserTally = request.body.scores
+        var newUserTally = request.body.scores;
         var tallyArray = [];
         var realFriendCount = 0;
         var bestMatch = 0;
         
-        for (var i = 0; i<newBestFriends.length; i++) {
+        for (var i = 0; i<friendsArray.length; i++) {
             var scoresDifference = 0;
 
             for (var l = 0; l<newUserTally; l++) {
-                scoresDifference += (Math.abs(parseInt(newBestFriends[i].scores[l]) - parseInt(newUserTally[j])));
+                scoresDifference += (Math.abs(parseInt(friendsArray[i].scores[l]) - parseInt(newUserTally[j])));
             }
             tallyArray.push(scoresDifference);
         } 
-        for(var i = 0; i<tallyArray.length; i++) {
-            if (tallyArray[i] <= tallyArray[bestMatch]) {
-                bestMatch = i;
+        for(var n = 0; n<tallyArray.length; n++) {
+            if (tallyArray[n] <= tallyArray[bestMatch]) {
+                bestMatch = n;
             }
         }
         var newBestFriend = friendsArray[bestMatch];
         response.json(newBestFriend);
 
-        friendsArray.push(req, body);
+        friendsArray.push(request, response);
     });
 };
