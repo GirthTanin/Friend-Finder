7b7dabf1da87248d9881c8981768ee615d10e35a
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-09-19 16:30:50 -0400

    now with actual images

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,7 +6,7 @@ var friends = [
     {
         name: "Wendy",
         // photo: "./public/images/Wendy(2).jpg",
-        photo: "https://m.media-amazon.com/images/M/MV5BMjM0OTIyMzY1M15BMl5BanBnXkFtZTgwMTg0OTE0NzE@._V1_SY1000_CR0,0,650,1000_AL_.jpg",
+        photo: "https://scontent.fphl2-4.fna.fbcdn.net/v/t31.0-8/21013922_10155494141671163_5389457343824643616_o.jpg?_nc_cat=0&oh=707d6d50970c6c4b2d7bc645ef2aa933&oe=5C279FAB",
         scores: [
             2,
             2,
@@ -23,7 +23,7 @@ var friends = [
     {
         name: "Nathan",
         // photo: "./public/images/Nathan.jpg",
-        photo: "https://m.media-amazon.com/images/M/MV5BMjM0OTIyMzY1M15BMl5BanBnXkFtZTgwMTg0OTE0NzE@._V1_SY1000_CR0,0,650,1000_AL_.jpg",
+        photo: "https://scontent.fphl2-4.fna.fbcdn.net/v/t31.0-8/12377949_10208298702734658_7979863970382589483_o.jpg?_nc_cat=0&oh=06022c4a52c048116b4d7e860e050e44&oe=5C340C8E",
         scores: [
             4,
             5,
@@ -40,7 +40,7 @@ var friends = [
     {
         name: "Samson",
         // photo: "./public/images/Samson.jpg",
-        photo: "https://m.media-amazon.com/images/M/MV5BMjM0OTIyMzY1M15BMl5BanBnXkFtZTgwMTg0OTE0NzE@._V1_SY1000_CR0,0,650,1000_AL_.jpg",
+        photo: "https://scontent.fphl2-4.fna.fbcdn.net/v/t1.0-9/29571390_10156127075181163_86508130121712663_n.jpg?_nc_cat=0&oh=538e6910be979f22ae9ba1a2c094a3d8&oe=5C2AF3FA",
         scores: [
             2,
             4,
@@ -57,7 +57,7 @@ var friends = [
     {
         name: "Sophia",
         // photo: "./public/imgages/Sophia.jpg",
-        photo: "https://m.media-amazon.com/images/M/MV5BMjM0OTIyMzY1M15BMl5BanBnXkFtZTgwMTg0OTE0NzE@._V1_SY1000_CR0,0,650,1000_AL_.jpg",
+        photo: "https://scontent.fphl2-4.fna.fbcdn.net/v/t1.0-9/299787_935246456443_530553516_n.jpg?_nc_cat=0&oh=62c78e9aee2ca08e025ac9f18fe1d921&oe=5C256BBE",
         scores: [
             4,
             3,
@@ -74,7 +74,7 @@ var friends = [
     {
         name: "Calvin",
         // photo: "./public/images/Calvin.jpg",
-        photo: "https://m.media-amazon.com/images/M/MV5BMjM0OTIyMzY1M15BMl5BanBnXkFtZTgwMTg0OTE0NzE@._V1_SY1000_CR0,0,650,1000_AL_.jpg",
+        photo: "https://scontent.fphl2-4.fna.fbcdn.net/v/t1.0-9/36488199_793476354374127_3563542375605207040_n.jpg?_nc_cat=0&oh=476d484e25bdc080a7024ba06bc68b54&oe=5C626C74",
         scores: [
             3,
             3,
@@ -91,7 +91,7 @@ var friends = [
     {
         name: "Cliona",
         // photo: "./public/images/Cliona.jpg",
-        photo: "https://m.media-amazon.com/images/M/MV5BMjM0OTIyMzY1M15BMl5BanBnXkFtZTgwMTg0OTE0NzE@._V1_SY1000_CR0,0,650,1000_AL_.jpg",
+        photo: "https://scontent.fphl2-4.fna.fbcdn.net/v/t1.0-9/36852216_10157794612439152_4862983790412169216_n.jpg?_nc_cat=0&oh=ade3030f95c7a9d4de6d6e30650d35f8&oe=5C62C639",
         scores: [
             3,
             5,
@@ -108,7 +108,7 @@ var friends = [
     {
         name: "yellow function",
         // photo: "./public/images/Nathan-Ulysses-Thadeus-Squirrel.jpg",
-        photo: "https://m.media-amazon.com/images/M/MV5BMjM0OTIyMzY1M15BMl5BanBnXkFtZTgwMTg0OTE0NzE@._V1_SY1000_CR0,0,650,1000_AL_.jpg",
+        photo: "https://scontent.fphl2-4.fna.fbcdn.net/v/t1.0-9/42186175_10156585997876163_5246451046263291904_n.jpg?_nc_cat=0&oh=16530b7bd1c96d548608b697e399f318&oe=5C1BD5EE",
         scores: [
             5,
             5,
